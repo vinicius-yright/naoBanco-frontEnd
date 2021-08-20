721f5483d7cb5921817e7301ee184fb769449a79
Vinicius Souza <vsdbpessoal@gmail.com>
2021-08-20 19:31:36 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Background>
+      <View style={styles.container}>
       <StatusBar style="auto" />
     </View>
+    </Background>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/Background/index.tsx
@@ -0,0 +1,21 @@
+import { theme } from '../../global/styles/theme';
+import React, {Children, ReactNode} from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import {  styles } from './styles';
+
+type Props = {
+    children: ReactNode;
+}
+
+
+export function Background({ children }: Props) {
+
+    return (
+        <LinearGradient
+            style={styles.container}
+            colors={[theme.colors.primary, theme.colors.secondary100]}
+        >
+              {children}
+        </LinearGradient>
+    )
+}
