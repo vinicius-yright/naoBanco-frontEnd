5d570a8589f05446e2d8e85c92ec8ae48376695c
Vinicius Souza <vsdbpessoal@gmail.com>
2021-09-17 20:24:44 -0300

    instalando fontes personalizadas e carregando-as no app.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import { StyleSheet, Text, View } from 'react-native';
 import { Background } from './src/components/Background';
+import AppLoading from 'expo-app-loading';
 import { Routes } from './src/routes';
 
 //import { CreateUser } from './src/screens/createUser';
 //import { FirstAccess } from './src/screens/firstAccess';
 
 export default function App() {
-  return (
-    <Background>
-      <View>
-        <StatusBar style="auto" />
-      </View>
 
-      <Routes />
-    </Background>
-  );
+  let [fontsLoaded] = useFonts({
+    Rajdhani_700Bold,
+    Rajdhani_500Medium,
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <Background>
+        <View>
+          <StatusBar style="auto" />
+        </View>
+
+        <Routes />
+      </Background>
+    );
+  }
 }
